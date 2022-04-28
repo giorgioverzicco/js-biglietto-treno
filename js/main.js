fd6efdad0d965a5a53c66f51073ba113e73492d7
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-04-28 14:06:53 +0200

    add style

--- js/main.js
+++ js/main.js
@@ -7,12 +7,19 @@ const age = Number(prompt("Quanti anni hai?"));
 const pricePerKm = km * 0.21;
 const discountForTeens = 0.2;
 const discountForElders = 0.4;
-let travelPrice = pricePerKm;
 
+let travelPrice = pricePerKm;
 if (age < 18) {
   travelPrice -= travelPrice * discountForTeens;
 } else if (age > 65) {
   travelPrice -= travelPrice * discountForElders;
 }
 
-console.log(travelPrice.toFixed(2));
+travelPrice = travelPrice.toFixed(2);
+
+// DOM manipulation
+document.getElementById("data").innerHTML += `Vuoi percorrere ${km} km...<br>`;
+document.getElementById("data").innerHTML += `ed hai ${age} anni!<br>`;
+document.getElementById("data").innerHTML += `Il prezzo del tuo biglietto è:`;
+
+document.getElementById("price").innerHTML = `${travelPrice}€`;
